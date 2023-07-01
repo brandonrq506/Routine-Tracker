1ace99fd7ae4743e33a4300bda9a771d0f7fe24b
Brandon Ramirez <darkrahd@gmail.com>
2023-07-01 11:13:26 -0600

    Fixed minor bug

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,4 +1,7 @@
 // import styles from './Timer.module.css';
+import { v4 as uuidv4 } from "uuid";
+import { findAvgTime, findCategory } from '../../utils/activityUtils'
+import { proper } from '../../utils/stringUtils';
 import { useContext } from 'react';
 import useTimer from '../../hooks/useTimer';
 import ItemsContext from '../../store/itemsContext'
@@ -13,12 +16,36 @@ const Timer = () => {
     }
 
     const handleNextTask = () => {
-        if (!nextTask) stop();
+        stop();
 
-        start(nextTask?.avgTime);
+        if (nextTask) {
+            start(nextTask.avgTime);
+        }
+
         itemsCtx.addAsDone({ ...currentTask, ...metaData });
         itemsCtx.removeToDo(currentTask.id);
     }
+
+    /**
+     * This does not work as addAsPriority inserts the item at the start of the array
+     * And the start of the array is our currentTask.
+     * Ideally we would insert the element as the second element of the array. So it is
+     * out next task in queue.
+     * To make this work we either update the addAsPriority function, or change how we
+     * handle our currentTask.
+     * We could potencially remove the currentTask from the to-do list and have it in a
+     * intermediary state.
+    */
+    const handlePriority = (event) => {
+        const todo = event.target.innerHTML
+        itemsCtx.addAsPriority({
+            id: uuidv4(),
+            name: proper(todo),
+            category: findCategory(todo),
+            avgTime: findAvgTime(todo),
+        });
+    }
+
 
 
     if (itemsCtx.toDoList.length === 0) {
@@ -35,6 +62,7 @@ const Timer = () => {
             <p>{percentage}%</p>
             <p>{metaData.duration}</p>
             {isRunning && <button onClick={handleNextTask}>Next</button>}
+            {isRunning && <button onClick={handlePriority}>Break</button>}
             <h2>Next Task: {nextTask?.name || 'No more tasks'}</h2>
         </>
     );

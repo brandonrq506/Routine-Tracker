6402c1fb3810bc8368cc105736fdf79c3bff1e11
Brandon Ramirez <darkrahd@gmail.com>
2023-07-03 23:39:08 -0600

    Re-design the TodoItem component

---
+++ src/components/ToDo/TodoItem/TodoItem.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import styles from './TodoItem.module.css';
+
+import DeleteButton from './DeleteButton';
+
+const TodoItem = ({ toDo }) => {
+    return (
+        <li className={styles[toDo.category]}>
+            <div className={styles.description}>
+                <p className={styles.avgTime}>{toDo.avgTime ?? '*'}</p>
+                <h4 className={styles.name}>{toDo.name}</h4>
+            </div>
+            <DeleteButton id={toDo.id} />
+        </li>
+    );
+};
+
+TodoItem.propTypes = {
+    toDo: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        avgTime: PropTypes.number,
+    }),
+    children: PropTypes.node,
+};
+
+export default TodoItem;

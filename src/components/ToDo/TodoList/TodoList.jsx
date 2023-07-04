61f1fccae3fbb3084668e2a67d12bb8b25128bff
Brandon Ramirez <darkrahd@gmail.com>
2023-07-03 23:42:18 -0600

    Added Todolist and Todo component

---
+++ src/components/ToDo/TodoList/TodoList.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+// import styles from './TodoList.module.css';
+
+import TodoItem from '../TodoItem/TodoItem';
+
+const TodoList = ({ toDos, header }) => {
+    const count = toDos.length;
+    let heading = header;
+    if (count > 0) {
+        const noun = count === 1 ? 'To-Do' : 'To-Dos';
+        heading = `${count} ${noun}`;
+    }
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            {toDos.map(todo =>
+                <TodoItem key={todo.id} toDo={todo} />
+            )}
+        </>
+    );
+};
+
+TodoList.propTypes = {
+    toDos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        avgTime: PropTypes.number,
+    })).isRequired,
+    header: PropTypes.string.isRequired,
+};
+
+export default TodoList;

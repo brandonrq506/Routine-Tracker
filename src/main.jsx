b8803e5d13e414225a38e9166a09df97012e55d6
Brandon Ramirez <darkrahd@gmail.com>
2023-07-03 23:44:45 -0600

    Delete unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ItemsProvider from './store/ItemsProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ItemsProvider>
+            <App />
+        </ItemsProvider>
+    </React.StrictMode>,
+);

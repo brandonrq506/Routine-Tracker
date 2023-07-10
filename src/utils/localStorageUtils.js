5d69fbf4d6853dc0b2b23fea10893725ea33c81a
Brandon Ramirez <darkrahd@gmail.com>
2023-07-10 12:19:20 -0600

    Improved our parseMethod to parse Dates propertly

--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -1,5 +1,23 @@
-export const parseInfo = (key) =>
-    JSON.parse(localStorage.getItem(key));
+export const parseInfo = (key) => {
+    const storedInfo = localStorage.getItem(key);
+    if (!storedInfo) return null
+    const parsedValue = JSON.parse(storedInfo);
+
+    if (Array.isArray(parsedValue)) {
+        return parsedValue.map(item => ({
+            ...item,
+            startTime: item.startTime ? new Date(item.startTime) : null,
+            endTime: item.endTime ? new Date(item.endTime) : null,
+        }));
+    }
+    else if (typeof parsedValue === 'object') {
+        return {
+            ...parsedValue,
+            startTime: parsedValue?.startTime ? new Date(parsedValue.startTime) : null,
+            endTime: parsedValue?.endTime ? new Date(parsedValue.endTime) : null,
+        };
+    }
+}
 
 export const stringifyInfo = (key, value) =>
     localStorage.setItem(key, JSON.stringify(value));
